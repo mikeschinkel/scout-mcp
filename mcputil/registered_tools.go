9a00d9473003dd7f4677734410f0e9c84cadaab9
Mike Schinkel <mike@newclarity.net>
2025-07-21 02:12:46 -0400

    Add `RegisteredToolsMap()`,` GetRegisteredTool()`

--- mcputil/registered_tools.go
+++ mcputil/registered_tools.go
@@ -1,11 +1,35 @@
 package mcputil
 
+import (
+	"strings"
+)
+
 var registeredTools []Tool
 
 func RegisteredTools() []Tool {
 	return registeredTools
 }
+func RegisteredToolsMap() (m map[string]Tool) {
+	m = make(map[string]Tool, len(registeredTools))
+	for _, tool := range registeredTools {
+		m[tool.Name()] = tool
+	}
+	return m
+}
 
 func RegisterTool(tool Tool) {
 	registeredTools = append(registeredTools, tool)
+}
+
+func GetRegisteredTool(name string) (t Tool) {
+	name = strings.ToLower(name)
+	for _, tool := range registeredTools {
+		if tool.Name() != name {
+			continue
+		}
+		t = tool
+		goto end
+	}
+end:
+	return t
 }
